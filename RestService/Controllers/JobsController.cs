33e3384bd0b6f77318d780f70793dfead6e78666
arobscotanf <filip.cotan@arobs.com>
2017-10-18 11:01:33 +0300

    Removed unused projects; added JSON provider instead of XML (default); created the JobsController with mock data

---
+++ RestService/Controllers/JobsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace CitySurfing.RestService.Controllers
+{
+    public class JobsController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new[] { "First job", "Second job" };
+        }
+
+        // GET: api/Jobs/5
+        public string Get(int id)
+        {
+            return "job";
+        }
+
+        // POST: api/Jobs
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Jobs/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Jobs/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
