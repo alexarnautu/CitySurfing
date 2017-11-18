cdd498fc6bc90e1edca96f0ef35e9b599dfac773
Filip Cotan <philipcotan@yahoo.com>
2017-11-18 23:17:45 +0200

    Started implementing repository pattern

---
+++ RestService/DAL/Repositories.cs
@@ -0,0 +1,38 @@
+﻿using CitySurfing.Domain.Models;
+using System;
+
+namespace CitySurfing.RestService.DAL
+{
+    public class UnitOfWork : IDisposable
+    {
+        private readonly AppDbContext _context = new AppDbContext();
+
+        private Repository<Skill> _skillsRepository;
+        public Repository<Skill> PaperRepository => _skillsRepository ?? (_skillsRepository = new Repository<Skill>(_context));
+
+        public void Complete()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool _disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ RestService/DAL/Repository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CitySurfing.RestService.DAL
+{
+    public class Repository<TEntity> where TEntity : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            params string[] includedProperties)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            query = includedProperties.Aggregate(query, (current, property) => current.Include(property));
+
+            return orderBy?.Invoke(query).ToList() ?? query.ToList();
+        }
+
+        public virtual TEntity GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public virtual void Create(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            TEntity entityToDelete = _dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if (_context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entityToDelete);
+            }
+            _dbSet.Remove(entityToDelete);
+        }
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            _dbSet.Attach(entityToUpdate);
+            _context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
