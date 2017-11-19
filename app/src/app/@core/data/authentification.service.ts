da2a76c3f3d1b5962f66d51e9162cdf3927b3425
Calin Salagean <salagean.calin@gmail.com>
2017-11-19 20:43:29 +0200

    Fixed linted errors

--- app/src/app/@core/data/authentification.service.ts
+++ app/src/app/@core/data/authentification.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, ResponseOptions } from '@angular/http';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map'
 
 @Injectable()
