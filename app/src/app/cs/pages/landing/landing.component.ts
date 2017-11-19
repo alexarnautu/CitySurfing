7a948cfd1bf0ac15d8cc08fcc36bce24eef70bdc
Alexandru Arnautu <alex.arnautu96@gmail.com>
2017-11-19 15:24:02 +0200

    modified landing page

--- app/src/app/cs/pages/landing/landing.component.ts
+++ app/src/app/cs/pages/landing/landing.component.ts
@@ -3,6 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'cs-landing-component',
     templateUrl: './landing.component.html',
+    styles: [`
+    .btn-xlarge {
+        padding: 18px 40px 45px;
+        margin: 20px;
+        font-size: 22px;
+        line-height: normal;
+        -webkit-border-radius: 8px;
+           -moz-border-radius: 8px;
+                border-radius: 8px;
+        }`,
+    `.big-text {
+        font-size: 20px;
+    }`],
 })
 export class LandingComponent {
 }
