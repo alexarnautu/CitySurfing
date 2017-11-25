dc4c6b73c9083d855126302a756b94d3d59bb951
Alexandru Arnautu <alex.arnautu96@gmail.com>
2017-11-25 16:45:11 +0200

    added editable style

--- app/src/app/cs/pages/listing/listing.component.ts
+++ app/src/app/cs/pages/listing/listing.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { StyleService } from '../../services/style.service';
 
 @Component({
   selector: 'cs-listing-component',
   templateUrl: './listing.component.html',
 })
 export class ListingComponent {
+  constructor(private styleService: StyleService) {
+    styleService.setStyle('no_background');
+  }
 
 }
